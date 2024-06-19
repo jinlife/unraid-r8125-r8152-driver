07583ebb9142ec553a937c26580c8146745403a9
Christoph Hummer <christophhummer@gmail.com>
2024-06-19 19:59:23 +0200

    add r8126 compiling stage

--- source/compile.sh
+++ source/compile.sh
@@ -5,6 +5,7 @@ cd ${DATA_DIR}/unraid-r8125-r8152-driver
 PLUGIN_VERSION="$(git log -1 --format="%cs" | sed 's/-//g')"
 git checkout main
 
+
 # Compile r8125 Kernel Module and install it to the temporary directory "/RTL8125"
 cd ${DATA_DIR}/unraid-r8125-r8152-driver/r8125/src
 
@@ -37,6 +38,44 @@ ${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME
 md5sum $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz.md5
 
 
+# Compile r8126 Kernel Module and install it to the temporary directory "/RTL8126"
+cd ${DATA_DIR}/unraid-r8125-r8152-driver/r8126/src
+
+# Compile Kernel Module and move it to a temporary directory
+make -j${CPU_COUNT}
+make INSTALL_MOD_PATH=/RTL8126 install -j${CPU_COUNT}
+
+# Remove non Kernel modules from temporary directory
+rm /RTL8126/lib/modules/${UNAME}/* 2>/dev/null
+
+# Create Slackware package
+PLUGIN_NAME="r8126"
+BASE_DIR="/RTL8126"
+TMP_DIR="/tmp/${PLUGIN_NAME}_"$(echo $RANDOM)""
+VERSION="$(date +'%Y.%m.%d')"
+
+mkdir -p $TMP_DIR/$VERSION
+cd $TMP_DIR/$VERSION
+cp -R $BASE_DIR/* $TMP_DIR/$VERSION/
+mkdir $TMP_DIR/$VERSION/install
+tee $TMP_DIR/$VERSION/install/slack-desc <<EOF
+       |-----handy-ruler------------------------------------------------------|
+$PLUGIN_NAME: $PLUGIN_NAME OOT driver by jinlife
+$PLUGIN_NAME:
+$PLUGIN_NAME: Source: https://github.com/jinlife/unraid-r8125-r8152-driver
+$PLUGIN_NAME:
+$PLUGIN_NAME: Custom $PLUGIN_NAME driver package for Unraid Kernel v${UNAME%%-*} by ich777
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+$PLUGIN_NAME:
+EOF
+${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz
+md5sum $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz.md5
+
+
 # Compile r8152 Kernel Module and install it to the temporary directory "/RTL8152"
 cd ${DATA_DIR}/unraid-r8125-r8152-driver/r8152/src
 
@@ -45,7 +84,6 @@ make INSTALL_MOD_PATH=/RTL8152 install -j${CPU_COUNT}
 
 # Remove non Kernel modules from temporary directory
 rm /RTL8152/lib/modules/${UNAME}/* 2>/dev/null
-
 
 # Create Slackware package
 PLUGIN_NAME="r8152"
@@ -74,6 +112,7 @@ EOF
 ${DATA_DIR}/bzroot-extracted-$UNAME/sbin/makepkg -l n -c n $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz
 md5sum $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz | awk '{print $1}' > $TMP_DIR/$PLUGIN_NAME-$PLUGIN_VERSION-$UNAME-1.txz.md5
 
+
 # Compile r8168 Kernel Module and install it to the temporary directory "/RTL8168"
 cd ${DATA_DIR}/unraid-r8125-r8152-driver/r8168/src
 
@@ -82,7 +121,6 @@ make INSTALL_MOD_PATH=/RTL8168 install -j${CPU_COUNT}
 
 # Remove non Kernel modules from temporary directory
 rm /RTL8168/lib/modules/${UNAME}/* 2>/dev/null
-
 
 # Create Slackware package
 PLUGIN_NAME="r8168"
